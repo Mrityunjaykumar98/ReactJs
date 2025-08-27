cc880346405964db1ada03454fbd7baaf35ce04f
Mrityunjay <mrityunjayer180@gmail.com>
2025-08-28 01:29:52 +0530

    added comment in usePrev

--- hooks/src/hooks/usePrev.js
+++ hooks/src/hooks/usePrev.js
@@ -6,6 +6,6 @@ export const usePrev = (value) =>{
     useEffect(()=>{
         ref.current = value
     },[value])
-
+//useEffect callback DOES NOT run before, but after this function returns.
     return ref.current // return first previous value of ref then useEffect get called and update ref current value
 }
